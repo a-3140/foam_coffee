381a59a974da944bc8243ecc54f3547eb5bbe9ee
a-3140 <aatienza.3140@gmail.com>
2022-05-13 13:58:14 +0800

    ui - initial design unfactored components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import bg from '../src/assets/bg.jpg'
+  const image: string = `url(${bg})`
+</script>
 
 <template>
   <main>
-    <router-view />
+    <div
+      v-bind:style="{ 'background-image': image }"
+      id="wrapper"
+      class="fullscreen overflow-hidden"
+    >
+      <router-view />
+    </div>
   </main>
 </template>
 
-<style></style>
+<style scoped>
+  #wrapper {
+    /* min-height: calc(100vh - 68px); */
+    background-repeat: no-repeat;
+    background-position: center;
+    background-attachment: fixed;
+    background-size: cover;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,86 @@
 <script setup lang="ts">
-  import HelloWorld from '../components/HelloWorld.vue'
   import { ref } from 'vue'
 
-  const count = ref(0)
+  const xMovement = ref('0')
+
+  function handleMouseMove(evt: any) {
+    const e = evt || window.event
+    const x = e.offsetX
+    xMovement.value = ((x - window.innerWidth / 2) / 10) as unknown as string
+    console.log('x', x)
+  }
 </script>
 
 <template>
-  <div class="min-h-full flex flex-col items-center justify-center">
-    <img alt="Vue logo" src="../assets/logo.png" class="mt-10" />
-    <button
-      @click="count++"
-      class="py-1 px-3 border text-sm font-medium rounded-md text-black"
-    >
-      Count: {{ count }}
-    </button>
-    <HelloWorld msg="Vite + Vue 3 + Typescript + Tailwind 3 + Vue Rounter 4" />
-    <div class="inline-flex rounded-md shadow mt-10">
-      <router-link
-        to="/about"
-        class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none"
-        >Next Page</router-link
+  <div
+    class="container mx-auto flex h-screen items-center justify-center py-5 px-6 md:py-0"
+    @mousemove="handleMouseMove"
+  >
+    <nav class="absolute top-0 flex w-screen justify-between py-4 px-10">
+      <div class="flex">
+        <svg
+          class="mr-2 -ml-1 mt-5 h-4 w-4"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fab"
+          data-icon="facebook-f"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 320 512"
+        >
+          <path
+            fill="currentColor"
+            d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"
+          ></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          class="mr-2 ml-3 mt-5 h-4 w-4"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fab"
+          data-icon="facebook-f"
+          role="img"
+        >
+          <path
+            d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+          />
+        </svg>
+      </div>
+
+      <img
+        alt="foam logo"
+        src="../assets/logo.png"
+        class="aspect-square item-center w-15 h-24"
+      />
+      <svg
+        class="mt-5 h-6 w-6"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
       >
+        <path
+          fill-rule="evenodd"
+          d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </nav>
+    <p
+      class="z-10 pb-80 text-6xl tracking-wide text-stone-900 antialiased sm:text-9xl pointer-events-none "
+    >
+      FOAM COFFEE
+    </p>
+    <div class="absolute bottom-0">
+      <img
+        alt="foam logo"
+        src="../assets/coffee.png"
+        :style="{
+          transform: `translateX(${xMovement}px`,
+          transition: 'transform ease-in-out',
+        }"
+      />
     </div>
   </div>
 </template>
