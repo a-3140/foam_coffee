d65db09aefde90eaddb57799a6cdaddd603759cd
a-3140 <aatienza.3140@gmail.com>
2022-05-19 19:18:39 +0800

    ui - add animation for menu icon

--- src/components/main/Nav.vue
+++ src/components/main/Nav.vue
@@ -2,10 +2,19 @@
   import BurgerIcon from '@/icons/burger.vue'
   import FacebookIcon from '@/icons/facebook.vue'
   import InstagramIcon from '@/icons/instagram.vue'
+  import CloseIcon from '@/icons/close.vue'
+  import { ref } from 'vue'
+
+  const isMenuOpen = ref(false)
+
+  const toggleMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value
+  }
 </script>
+
 <template>
   <nav
-    class="nav-menu fixed top-0 left-0 z-40 flex w-screen flex-row py-4 px-10 transition delay-150 ease-in-out"
+    class="nav-menu fixed top-0 left-0 z-40 flex w-screen flex-row py-4 px-20 transition delay-150 ease-in-out"
   >
     <div class="flex basis-1/3">
       <a href="https://www.facebook.com/foamcoffeeph/" target="_blank">
@@ -24,7 +33,12 @@
       />
     </div>
     <div class="flex basis-1/3 justify-end">
-      <burger-icon />
+      <div class="cursor-pointer" @click="toggleMenu">
+        <transition name="fade" appear>
+          <burger-icon v-if="!isMenuOpen" class="absolute" />
+          <close-icon v-else="!sMenuOpen" class="absolute" />
+        </transition>
+      </div>
     </div>
   </nav>
 </template>
@@ -37,5 +51,14 @@
       rgba(255, 255, 255, 0.35),
       rgba(255, 255, 255, 0)
     );
+  }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
   }
 </style>
