bbeb5ba926d157cb4b6eee3aa8c1351b0091a806
a-3140 <aatienza.3140@gmail.com>
2022-05-21 11:57:51 +0800

    tech - minor refactor for readability and consistent naming of cloud class from 'bubble' class

--- src/components/animated/AnimatedCloud.vue
+++ src/components/animated/AnimatedCloud.vue
@@ -13,33 +13,32 @@
   type CloudArray = Cloud[]
 
   const cloudArray: CloudArray = new Array(7).fill({}).map((e, i) => {
+    const initialSize = 400
+    const initialDelay = 0.4
     const sizeDiff = i * 40
-    const additionalDelay = i * 0.05
+    const additionalDelay = i * 0.1
     return {
-      size: 400 - sizeDiff,
-      transitionDelay: 0.2 + additionalDelay,
+      size: initialSize - sizeDiff,
+      transitionDelay: initialDelay + additionalDelay,
     }
   })
 
-  const cloudStyle = ref<CloudStyle>({ scale: 0, opacity: 0 })
-
-  const fadeoutCloud = (): void => {
+  function fadeoutCloud(): void {
     cloudStyle.value = { opacity: 0, scale: 1 }
   }
-
-  const fadeinExpand = (): void => {
+  function fadeinExpand(): void {
     cloudStyle.value = { scale: 1, opacity: 1 }
+  }
+  function animateClouds(): void {
+    fadeinExpand()
+    clearTimeout(delayBeforeFadeout)
+    delayBeforeFadeout = setTimeout(fadeoutCloud, 450)
   }
 
   let delayBeforeFadeout = 0
-  const animateClouds = () => {
-    fadeinExpand()
-    clearTimeout(delayBeforeFadeout)
-    delayBeforeFadeout = setTimeout(fadeoutCloud, 200)
-  }
-
-  const cloudCoordinates = useCursorCoordinates()
   const showCloudCursor = ref(false)
+  const cloudCoordinates = useCursorCoordinates()
+  const cloudStyle = ref<CloudStyle>({ scale: 0, opacity: 0 })
 
   watch(cloudCoordinates, () => {
     if (!showCloudCursor.value) {
@@ -52,7 +51,7 @@
 <template>
   <div
     v-for="cloud in cloudArray"
-    class="bubble absolute top-0 left-0 z-30 cursor-pointer blur-3xl"
+    class="cloud absolute top-0 left-0 z-30 cursor-pointer blur-3xl"
     :key="cloud.size"
     :hidden="!showCloudCursor"
     :style="{
@@ -68,7 +67,7 @@
 </template>
 
 <style scoped>
-  .bubble {
+  .cloud {
     background: radial-gradient(circle closest-side, #ffffff, transparent);
   }
 </style>
